fb5194681fcd129214cec197bb63e44ee4b87aa8
Himani Suresh Rothey <himanirothey200@gmail.com>
2023-08-09 11:08:37 +0530

    Created Job Fair Q1ans and Job Fair Q2ans added

---
+++ src/main/java/com/Salaryfy/Interfaces/IJobfairQ1ans.java
@@ -0,0 +1,18 @@
+package com.Salaryfy.Interfaces;
+
+import com.Salaryfy.Dto.JobfairQ1ans.JobfairQ1ansDto;
+import com.Salaryfy.Entity.JobfairQ1ans;
+
+import java.util.List;
+
+public interface IJobfairQ1ans {
+
+    public String addQ1ans(JobfairQ1ansDto jobfairQ1ansDto);
+    public JobfairQ1ans getQ1ans(Integer jobFairQ1Id);
+    public String deleteQ1ans(Integer jobFairQ1Id);
+
+
+
+
+
+}
---
+++ src/main/java/com/Salaryfy/Repository/JobfairQ2ansRepo.java
@@ -0,0 +1,10 @@
+package com.Salaryfy.Repository;
+
+import com.Salaryfy.Entity.JobfairQ2ans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobfairQ2ansRepo extends JpaRepository<JobfairQ2ans,Integer> {
+
+}
