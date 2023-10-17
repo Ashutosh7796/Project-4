6575a175cf0d5541e92079591eff660dbeefa7c3
tkraj888 <113091344+tkraj888@users.noreply.github.com>
2023-10-17 17:39:31 +0530

    okkk

--- src/main/java/com/Salaryfy/Config/AppConfig.java
+++ src/main/java/com/Salaryfy/Config/AppConfig.java
@@ -124,6 +124,7 @@ public class AppConfig {
                 CorsConfiguration config = new CorsConfiguration();
 //                config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
                 config.setAllowedOrigins(Collections.singletonList("http://64.227.186.1:5173"));
+                config.setAllowedOrigins(Collections.singletonList("http://146.190.8.175:5173"));
                 config.setAllowedMethods(Collections.singletonList("*"));
                 config.setAllowCredentials(true);
                 config.setAllowedHeaders(Collections.singletonList("*"));
