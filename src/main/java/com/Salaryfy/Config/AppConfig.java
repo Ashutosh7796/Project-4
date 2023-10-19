362b1b4335d3c34c2e626034355cb41aec7db00a
tkraj888 <113091344+tkraj888@users.noreply.github.com>
2023-10-19 14:46:54 +0530

    lolo

--- src/main/java/com/Salaryfy/Config/AppConfig.java
+++ src/main/java/com/Salaryfy/Config/AppConfig.java
@@ -85,7 +85,7 @@ public class AppConfig {
                 .requestMatchers("/ESuggest/**").permitAll()
                 .requestMatchers("/Interview/**").hasAuthority("USER")
                 .requestMatchers("/payment/**").hasAuthority("USER")
-                .requestMatchers("/profileLevel/**").permitAll()
+                .requestMatchers("/profileLevel/**").hasAuthority("USER")
                 .requestMatchers("/verification/**").permitAll()
                 .requestMatchers("/company/**").permitAll()
                 .requestMatchers("/experience/**").hasAuthority("USER")
